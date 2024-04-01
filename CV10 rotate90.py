2596699645862796fc09cc430e4fe422e1693b82
bhxrath <122245298+bhxrath@users.noreply.github.com>
2024-04-01 08:52:33 +0530

    Create CV10 rotate90.py

---
+++ CV10 rotate90.py
@@ -0,0 +1,9 @@
+import cv2
+path = r"C:/Users/Admin/Pictures/craiyon_084318_Cartoon_drawing_of_a_cute_panda.jpg"
+src = cv2.imread(path)
+window_name = 'Image'
+image = cv2.rotate(src, cv2.ROTATE_90_COUNTERCLOCKWISE)
+# Displaying the image
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
+ 7 changes: 7 additions & 0 deletions7  
